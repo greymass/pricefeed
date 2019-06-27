556c7fa03867a94c24200d4f24dfd210779de465
Johan Nordberg <its@johan-nordberg.com>
2019-06-27 03:08:29 +0200

    Fix broken eosjs typings

---
+++ src/eosfix.ts
@@ -0,0 +1,9 @@
+/* tslint:disable: no-empty-interface */
+
+// Workaround for undefined typings in eosjs, not sure what's going on here
+// Prob related to them using dependency injection for the encoder and fetch libraries
+interface Request {}
+interface RequestInit {}
+interface Response {}
+interface TextDecoder {}
+interface TextEncoder {}
