b15a5b3ecb30aa8de5c568f65896e8014a0ce77a
Aaron Cox <aaron@greymass.com>
2020-06-19 13:15:31 -0400

    Removed multiplication

--- src/providers/newdex.ts
+++ src/providers/newdex.ts
@@ -31,7 +31,7 @@ export default class NewdexProvider implements PriceProvider {
         return [
             { pair: 'eosemt', volume: emt.volume, price: emt.price },
             { pair: 'vigeos', volume: vig.volume, price: vig.price * 1e4 },
-            { pair: 'eosvigor', volume: vigor.volume, price: vigor.price * 1e4 },
+            { pair: 'eosvigor', volume: vigor.volume, price: vigor.price },
         ]
     }
 }
