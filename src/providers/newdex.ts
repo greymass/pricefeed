548bead8647f7afea133cc3c3292d600ec5c6ada
Aaron Cox <aaron@greymass.com>
2020-06-19 13:12:44 -0400

    Added eosvigor pair

--- src/providers/newdex.ts
+++ src/providers/newdex.ts
@@ -23,13 +23,15 @@ export default class NewdexProvider implements PriceProvider {
     }
 
     async run() {
-        const [emt, vig] = await Promise.all([
+        const [emt, vig, vigor] = await Promise.all([
             this.getPair('emanateoneos-emt-eos'),
             this.getPair('vig111111111-vig-eos'),
+            this.getPair('eosio.token-eos-vigor'),
         ])
         return [
             { pair: 'eosemt', volume: emt.volume, price: emt.price },
             { pair: 'vigeos', volume: vig.volume, price: vig.price * 1e4 },
+            { pair: 'eosvigor', volume: vigor.volume, price: vigor.price * 1e4 },
         ]
     }
 }
